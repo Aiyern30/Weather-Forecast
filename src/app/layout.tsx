bfe8c3c9381cd78ff047c4471865e6db87130ca1
Aiyern30 <ianbian2@gmail.com>
2024-08-21 12:19:05 +0800

    xx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,16 +5,15 @@ import { useEffect, useState } from "react";
 import "./globals.css";
 import { Sidebar } from "./components/sidebar";
 import { ThemeProvider } from "./components/theme-provider";
-import { QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { LoginProvider } from "./LoginContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
-  const [queryClient, setQueryClient] = useState(null);
+  const [queryClient, setQueryClient] = useState<QueryClient | null>(null);
 
   useEffect(() => {
-    const { QueryClient, QueryClientProvider } = require("react-query");
     const client = new QueryClient();
     setQueryClient(client);
 
@@ -23,29 +22,26 @@ const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
     };
   }, []);
 
+  if (!queryClient) return <div>Loading...</div>;
+
   return (
     <html lang="en">
-      <body
-        className="flex gap-[25px] p-[25px] dark:bg-black"
-        suppressHydrationWarning={true}
-      >
-        {queryClient && (
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <QueryClientProvider client={queryClient}>
-              <LoginProvider>
-                <Sidebar />
-                <div className="bg-[rgb(246,246,248)] w-full rounded-xl p-[30px] overflow-auto min-w-[1600px] dark:bg-slate-800">
-                  {children}
-                </div>
-              </LoginProvider>
-            </QueryClientProvider>
-          </ThemeProvider>
-        )}
+      <body className="flex gap-[25px] p-[25px] dark:bg-black">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <QueryClientProvider client={queryClient}>
+            <LoginProvider>
+              <Sidebar />
+              <div className="bg-[rgb(246,246,248)] w-full rounded-xl p-[30px] overflow-auto min-w-[1600px] dark:bg-slate-800">
+                {children}
+              </div>
+            </LoginProvider>
+          </QueryClientProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
